f5b88ecd578227e68efd5d947b677b4a8e9692b3
邓润庭 <1032939141@qq.com>
2019-10-10 23:08:29 +0800

    将当前用户添加到docker用户组

--- shell_scripts/install_docker.sh
+++ shell_scripts/install_docker.sh
@@ -1,5 +1,5 @@
 # step 1: 安装必要的一些系统工具
-sudo apt-get update
+sudo apt-get update && \
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 # step 2: 安装GPG证书
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
@@ -8,10 +8,10 @@ sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/li
 # Step 4: 更新并安装Docker-CE
 sudo apt-get -y update
 sudo apt-get -y install docker-ce
-# 安装指定版本的Docker-CE:
-# Step 1: 查找Docker-CE的版本:
-# apt-cache madison docker-ce
-#   docker-ce | 17.03.1~ce-0~ubuntu-xenial | http://mirrors.aliyun.com/docker-ce/linux/ubuntu xenial/stable amd64 Packages
-#   docker-ce | 17.03.0~ce-0~ubuntu-xenial | http://mirrors.aliyun.com/docker-ce/linux/ubuntu xenial/stable amd64 Packages
-# Step 2: 安装指定版本的Docker-CE: (VERSION例如上面的17.03.1~ce-0~ubuntu-xenial)
-# sudo apt-get -y install docker-ce=[VERSION]
+
+# 将当前用户添加到docker用户组，避免使用sudo
+sudo gpasswd -a ${USER} docker
+# 重启docker
+sudo systemctl restart docker
+# 如果执行命令还是出现权限问题
+sudo chmod a+rw /var/run/docker.sock
