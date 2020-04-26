8bc1e54c0690ba34814250ac5e676878c3e82fdd
邓润庭 <dengrt@akulaku.com>
2020-04-26 16:35:39 +0800

    update：优化实现方式

--- python_scripts/python_dict_impliment.py
+++ python_scripts/python_dict_impliment.py
@@ -1,155 +1,193 @@
 import os
 import math
+import unittest
 
+ENTRY_STATUS_DUMMY = "asjddh"  # 标识me_key为dummy状态
+
+
+class DictEntry(object):
+
+    def __init__(self, **kwargs):
+        self.me_key = kwargs.get("me_key")
+        self.me_hash = kwargs.get("me_hash") or (hash(self.me_key) if self.me_key is not None else self.me_key)
+        self.me_value = kwargs.get("me_value")
+
+    def from_new(self, new_slot):
+        self.me_key = new_slot.me_key
+        self.me_hash = new_slot.me_hash
+        self.me_value = new_slot.me_value
+
+    @property
+    def is_active(self):
+        return True if (self.me_key is not None and self.me_value is not None and self.me_key != ENTRY_STATUS_DUMMY) else False
+
+    @property
+    def is_unused(self):
+        return True if self.me_key == self.me_value == None else False
+
+    @property
+    def is_dummy(self):
+        return True if self.me_key == ENTRY_STATUS_DUMMY and self.me_value is None else False
+
+    def __str__(self):
+        return "{}: {}".format(self.me_key, self.me_value)
 
 class DictObj(object):
-    default_size = 8 
+    min_size = 8 
 
     def __init__(self):
-        # [stauts, key, value]
-        # status 0-未使用，1-已使用，2-已删除
-        self.size = self.default_size
-        self.data_list = self._genrate_data_list()
+        self.ma_smalltable = self._generate_table(self.min_size)  # 创建字典对象时，一定会创建一个大小为PyDict_MINSIZE==8的PyDictEntry数组。
+        self.ma_fill = 0  # 所有处于Active以及Dummy的元素个数
+        self.ma_used = 0 # 所有处于Active状态的元素个数
+        self.ma_mask = self.min_size # 所有entry的元素个数（Active+Dummy+Unused）
+        self.ma_table = self.ma_smalltable
     
-    def _genrate_data_list(self):
-        return [[0, None, None] for _ in range(self.size)]
+    def _generate_table(self, size):
+        return [DictEntry() for _ in range(size)]
 
-    def _probing(self, j):
+    def _probing(self, j, size):
         # 探测方法
-        i = math.log(self.size, 2)
+        i = math.log(size, 2)
         return int(((5*j)+1) % 2**i)
 
     def __setitem__(self, key, value):
         # 检查占用率
-        if(self._full_rate()> 2/3):  # 扩容
+        if(self.ma_used / self.ma_mask > 2/3):  # 扩容
             self._expand_datalist()
-        key_hash = hash(key)
-        origin_index = key_hash & self.size - 1
-        index = self._get_insert_index(origin_index, key_hash)
-        self.data_list[index] = [1, key, value]
-        print("key: {}, orgin_index: {}, index: {}".format(key, origin_index, index))
-        return index
+        index = self._look_dict(key)
+        slot = self.ma_table[index]
+        if slot.is_active:
+            slot.me_value = value
+        elif slot.is_dummy or slot.is_unused:
+            if slot.is_unused:
+                self.ma_fill += 1
+            self.ma_used += 1
+            slot.from_new(DictEntry(me_key=key, me_value=value))
+        # print("key: {}, index: {}".format(key, index))
+        
 
     def __getitem__(self, key):
-        key_hash = hash(key)
-        origin_index = key_hash & self.size - 1
-        index = self._get_find_index(origin_index, key_hash)
-        data = self.data_list[index]
-        if data[0] in (0, 2):
-            raise Exception("KeyError: {}".format(key))
-        return self.data_list[index]
-
-    def _full_rate(self):
-        return self._used_slots() / self.size
-
-    def _used_slots(self):
-        return len([_ for _ in self.data_list if _[0] in (1,2)])
-
-    def _filled_slots(self):
-        return len([_ for _ in self.data_list if _[0] == 1])
+        index = self._look_dict(key)
+        slot = self.ma_table[index]
+        if slot.is_active:
+            return slot.me_value
+        else:
+            raise Exception("Key error! [{}]".format(key))
 
     def _expand_datalist(self):
         print("---扩容---")
-        old_data_list = self.data_list
-        i = int(math.log(self.size, 2))
-        expand_num = 2 if self._used_slots() > 50000 else 4
+        old_table = self.ma_table
+        i = int(math.log(self.ma_mask, 2))
+        expand_num = 2 if self.ma_used > 50000 else 4
         while True:
             i += 1
             new_size = 2 ** i
-            if new_size >= expand_num * self._used_slots():
-                self.size = new_size
-                self.data_list = self._genrate_data_list()
+            if new_size >= expand_num * self.ma_used:
+                self.ma_mask = new_size
+                self.ma_table = self._generate_table(new_size)
                 break
         # 将以前的数据迁移过去
-        for x in old_data_list:
-            if x[0] == 1:
-                self[x[1]] = x[2]
+        self.ma_fill = self.ma_used = 0
+        for old_slot in old_table:
+            if old_slot.is_active:
+                # self._process_insert(old_slot.me_hash & self.ma_mask - 1, old_slot)
+                self[old_slot.me_key] = old_slot.me_value
+    
+    def _look_dict(self, key):
+        key_hash = hash(key)
+        origin_index = key_hash & self.ma_mask - 1
+        # print("key: {}, origin_index: {}".format(key, origin_index))
+        return self._real_look_dict(origin_index, key_hash)
 
-    def _get_insert_index(self, index, key_hash):
-        status, k, v = self.data_list[index]
-        if status == 0:  # 空slot直接插入
-            pass
-        elif status in (1, 2):  # 占用或dummy状态，探测下一个
-            if status == 1 and key_hash == hash(k):
-                pass
-            else:
-                prob_index = self._probing(index)
-                index = self._get_insert_index(prob_index, key_hash)
-        else:
-            raise Exception("Wrong status: {}".format(status))
-        return index
-
-    def _get_find_index(self, index, key_hash):
-        status, k, v = self.data_list[index]
-        if status == 0:  # 空slot直接返回
-            pass
-        elif status in (1, 2):  # 占用或dummy状态，探测下一
-            if key_hash != hash(k):
-                index = self._get_find_index(self._probing(index), key_hash)
-        else:
-            raise Exception("Wrong status: {}".format(status))
+    def _real_look_dict(self, index, key_hash):
+        slot = self.ma_table[index]
+        if slot.is_active:  # active
+            if slot.me_hash != key_hash:
+                next_index = self._probing(index, self.ma_mask)
+                index = self._real_look_dict(next_index, key_hash)
         return index
     
     def __str__(self):
-        return "{}".format({x[1]:x[2] for x in self.data_list if x[0] == 1})
+        return "{}".format({slot.me_key:slot.me_value for slot in self.ma_table if slot.me_key is not None and slot.me_value is not None})
     
     def items(self):
-        return [[x[1], x[2]] for x in self.data_list if x[0] == 1]
+        return [(slot.me_key, slot.me_value) for slot in self.ma_table if slot.is_active]
 
     def keys(self):
-        return [x[1] for x in self.data_list if x[0] == 1]
+        return [slot.me_key for slot in self.ma_table if slot.is_active]
 
     def values(self):
-        return [x[2] for x in self.data_list if x[0] == 1] 
+        return [slot.me_value for slot in self.ma_table if slot.is_active]
 
     def __len__(self):
-        return self._filled_slots()
+        return self.ma_used
 
     def __delitem__(self, key):
-        key_hash = hash(key)
-        origin_index = key_hash & self.size - 1
-        index = self._get_find_index(origin_index, key_hash)
-        data = self.data_list[index]
-        if data[0] in (0, 2):
-            raise Exception("KeyError: {}".format(key))
+        index = self._look_dict(key)
+        slot = self.ma_table[index]
+        if slot.is_active:
+            slot.me_key = ENTRY_STATUS_DUMMY
+            slot.me_value = None
+            self.ma_used -= 1
         else:
-            self.data_list[index] = [2, data[1], data[2]]
+            raise Exception("Key error! [{}]".format(key))
+
+
+class TextDictObj(unittest.TestCase):
+
+    def set_values(self):
+        d = DictObj()
+        d["a"] = "name_a"
+        d["b"] = "name_b"
+        d["c"] = "name_c"
+        d["d"] = "name_d"
+        d["e"] = "name_e"
+        d["f"] = "name_f"
+        d[1] = "name_1"
+        d[2] = "name_2"
+        d[3] = "name_3"
+        d[4] = "name_4"
+        d[5] = "name_5"
+        d[6] = "name_6"
+        d[7] = "name_7"
+        d[8] = "name_8"
+        d[9] = "name_9"
+        d[10] = "name_10" 
+        return d
+
+    def test_set(self):
+        d = DictObj()
+        d["a"] = "name_a"
+        
+    def test_get(self):
+        d = DictObj()
+        d["a"] = "name_a"
+        self.assertEqual(d["a"], "name_a")
+
+    def test_keys(self):
+        d = self.set_values()
+        print(d.keys())
+
+    def test_items(self):
+        d = self.set_values()
+        print(d.items())
+
+    def test_values(self):
+        d = self.set_values()
+        print(d.values())
+    
+    def test_del(self):
+        d = self.set_values()
+        del d["a"]
+        with self.assertRaises(BaseException):
+            d["a"]
+
+    def test_len(self):
+        d = self.set_values()
+        l1 = len(d)
+        del d["a"]
+        l2 = len(d)
+        self.assertEqual(l1-l2, 1)
 
 if __name__ == "__main__":
-    d = DictObj()
-    d["a"] = "name_a"
-    d["b"] = "name_b"
-    d["c"] = "name_c"
-    d["d"] = "name_d"
-    d["e"] = "name_e"
-    d["f"] = "name_f"
-    d[1] = "name_1"
-    d[2] = "name_2"
-    d[3] = "name_3"
-    d[4] = "name_4"
-    d[5] = "name_5"
-    d[6] = "name_6"
-    d[7] = "name_7"
-    d[8] = "name_8"
-    d[9] = "name_9"
-    d[10] = "name_10"
-    # print(d)
-    # print(d[9])
-    # print(d["a"])
-    # print(d["b"])
-    # print(d["c"])
-    # print(d["d"])
-    # print(d["e"])
-    # print(d["f"])
-    # print(d["g"])
-    d["a"] = "name_a_2"
-    d[10] = "name_10_2"
-
-
-    # print(len(d))
-    # del d["x"]
-    # print(len(d))
-    # print(d.items())
-    print(d)
-    # print(d.keys())
-    # print(d.values())
+    unittest.main()
